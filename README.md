5d97ec03ba6bb37866b523e68066110062f921b7
Alex Ranc <alex.ranc@hear.com>
2021-09-24 12:01:10 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# 🔌 React-Folder-Component ✨
+
+### Scaffolds a React Folder Component
+
+```
+-ComponentFolder/
+---index.js
+---style.scss
+```
+
+```
+-ComponentFolder/
+---index.js
+---style.module.scss
+```
---
+++ utils.js
@@ -0,0 +1,85 @@
+'use strict';
+const vscode = require('vscode');
+const fse = require('fs-extra');
+const fs = require('fs');
+const path = require('path');
+const pascalCase = require('change-case').pascalCase;
+
+function logger(type, msg = '') {
+  switch (type) {
+    case 'success':
+      return vscode.window.setStatusBarMessage(`Success: ${msg}`, 5000);
+    case 'warning':
+      return vscode.window.showWarningMessage(`Warning: ${msg}`);
+    case 'error':
+      return vscode.window.showErrorMessage(`Failed: ${msg}`);
+  }
+}
+
+module.exports = {
+  logger,
+  generators: {
+    templatesDir: path.join(__dirname, '/templates'),
+
+    createFile: (file, data) =>
+      new Promise(resolve => {
+        let output = fse.outputFile(file, data);
+        resolve(output);
+      }),
+
+    resolveWorkspaceRoot: path =>
+      path.replace('${workspaceFolder}', vscode.workspace.rootPath),
+
+    createComponentDir: function(uri, componentName) {
+      let contextMenuSourcePath;
+
+      if (uri && fs.lstatSync(uri.fsPath).isDirectory()) {
+        contextMenuSourcePath = uri.fsPath;
+      } else if (uri) {
+        contextMenuSourcePath = path.dirname(uri.fsPath);
+      } else {
+        contextMenuSourcePath = vscode.workspace.rootPath;
+      }
+
+      let componentDir = `${contextMenuSourcePath}/${pascalCase(
+        componentName
+      )}`;
+      fse.mkdirsSync(componentDir);
+
+      return componentDir;
+    },
+
+    createComponent: function(componentDir, componentName, type) {
+      let templateFileName = this.templatesDir + `/${type}.template`;
+
+      const compName = pascalCase(componentName);
+
+      let componentContent = fs
+        .readFileSync(templateFileName)
+        .toString()
+        .replace(/{componentName}/g, compName);
+
+      let filename = `${componentDir}/index.js`;
+
+      return this.createFile(filename, componentContent);
+    },
+
+    createCSS: function(componentDir, componentName, type) {
+      let templateFileName = `${this.templatesDir}/scss.template`;
+
+      const compName = pascalCase(componentName);
+      let cssContent = fs
+        .readFileSync(templateFileName)
+        .toString()
+        .replace(/{componentName}/g, compName);
+
+      let filename = `${componentDir}/style.scss`;
+
+    //   if (type === 'functionalCssModule') {
+    //       filename = `${componentDir}/style.module.scss`;
+    //   }
+
+      return this.createFile(filename, cssContent);
+    }
+  }
+};
